0e91619b64ed9c88ad4691acf6f222d3fdb238ba
ashinkaranasinghe <47234272+ashinkaranasinghe@users.noreply.github.com>
2019-06-26 19:50:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Button } from "react-native";
+import { SSL_OP_NO_TLSv1_2 } from "constants";
 
 const instructions = "Ashinka shani";
 
@@ -9,27 +10,35 @@ export default class App extends Component {
   };
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ width: 50, height: 50, backgroundColor: "powderblue" }}>
-          <Button
-            onPress={this.onPresentLearnMore}
-            title="Learn More"
-            color="#841584"
-          />
-        </View>
-        <View
-          style={{
-            flex: 2,
-            width: 100,
-            height: 100,
-            backgroundColor: "skyblue"
-          }}
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column"
+        }}
+      >
+        <Button
+          onPress={this.onPresentLearnMore}
+          title="Learn More"
+          color="#841584"
         />
         <View
           style={{
-            flex: 3,
-            width: 150,
-            height: 150,
+            marginTop: 20,
+            justifyContent: "center",
+            alignItems: "stretch",
+            width: 50,
+            height: 50,
+            backgroundColor: "powderblue"
+          }}
+        />
+        <View
+          style={{ flex: 1, width: 50, height: 50, backgroundColor: "skyblue" }}
+        />
+        <View
+          style={{
+            flex: 2,
+            width: 50,
+            height: 50,
             backgroundColor: "steelblue"
           }}
         />
@@ -53,8 +62,6 @@ const styles = StyleSheet.create({
   },
   button: {
     marginLeft: 150,
-    marginTop: 150,
-    width: 100,
-    Text_color: "#143333"
+    marginTop: 150
   }
 });
