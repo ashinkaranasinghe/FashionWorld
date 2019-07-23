e2659512b2ba3b38906aa6702008d4d0c4b8231c
ashinkaranasinghe <47234272+ashinkaranasinghe@users.noreply.github.com>
2019-07-23 07:20:21 +0530

    store data

--- screens/tabone.js
+++ screens/tabone.js
@@ -52,17 +52,6 @@ class tabone extends Component {
       // Error saving data
     }
   };
-  retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("TASKS");
-      if (value !== null) {
-        // We have data!!
-        console.log(value);
-      }
-    } catch (error) {
-      // Error retrieving data
-    }
-  };
 
   static navigationOptions = {
     title: "News",
